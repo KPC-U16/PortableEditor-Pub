5e86b9cca1de608386355d32f7ac1e4a6cefa2ca
noko1024-mac <ynoko1024@gmsil.com>
2021-10-25 14:11:30 +0900

    Fix spell miss

---
+++ Editor/vscodeInstaller.bat
@@ -0,0 +1,32 @@
+@echo off
+chcp 932
+set dir=%~dp0
+
+tasklist /fo csv /nh /fi "IMAGENAME eq Code.exe" | find "Code.exe" > NUL
+
+if NOT ERRORLEVEL 1 (
+
+	rem メッセージ表示
+	echo MsgBox "VSCode が起動中です。",vbInformation,"VSCodeInstaller" > %TEMP%\msgbox.vbs & %TEMP%\msgbox.vbs
+
+	rem ファイル削除
+	del /Q %TEMP%\msgbox.vbs
+
+) ELSE (
+
+	"../Ruby/bin/ruby.exe" ./setting/vscodeInstaller.rb %dir:~0,-1%
+	cd ./vscode
+
+	start Code.exe ../../Src
+	echo -----------------
+	echo 再起動処理中
+	echo -----------------
+	waitfor dummy /t 10>nul 2>&1 & verify>nul
+	taskkill /im Code.exe
+  
+	echo MsgBox "セットアップが完了しました。",vbInformation,"VSCodeInstaller" > %TEMP%\msgbox.vbs & %TEMP%\msgbox.vbs
+	del /Q %TEMP%\msgbox.vbs
+
+)
+
+exit
