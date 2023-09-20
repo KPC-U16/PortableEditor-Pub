34d845037285839266291d4e8db32e54fa1d25ce
yugu0202 <49861024+yugu0202@users.noreply.github.com>
2023-09-20 16:16:52 +0900

    require, includeの追加とコードの修正

--- Src/practice.rb
+++ Src/practice.rb
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-require  'CHaserConnect.rb' #呼び出すおまじない
+require 'CHaserConnect.rb' #呼び出すおまじない
+require 'CHaserTools.rb'
+
+include Action
+include Direction
+include MapInfo
 
 # 書き換えない
 target = CHaserConnect.new("prac") # ()の中好きな名前
@@ -18,14 +23,6 @@ loop do # ここからループ
 #-----ここまで書き換えない-----
 
 #ここに処理を書く
-
-#values = target.walkUp
-if values[2] == 2
-  values = target.walkRight
-else 
-  values = target.walkUp
-end
-
 
 #---------ここから---------
   if values[0] == 0
