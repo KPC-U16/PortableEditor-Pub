d9b750bccfe6858320075faba374276ffbef4cd6
umiwatarin <kaito.946nct@gmail.com>
2023-09-20 09:14:19 +0900

    fix require

--- Src/practice.rb
+++ Src/practice.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-require  'CHaserConnect.rb' #呼び出すおまじない
+#require  'CHaserConnect.rb' #呼び出すおまじない
+require  '.\CHaserConnect.rb' #呼び出すおまじない
 
 # 書き換えない
 target = CHaserConnect.new("prac") # ()の中好きな名前
@@ -18,6 +19,14 @@ loop do # ここからループ
 #-----ここまで書き換えない-----
 
 #ここに処理を書く
+
+#values = target.walkUp
+if values[2] == 2
+  values = target.walkRight
+else 
+  values = target.walkUp
+end
+
 
 #---------ここから---------
   if values[0] == 0
